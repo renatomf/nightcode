bb963fc8afc558ec0a61e7b3e3b1ea18e97ac265
renatomf <renato@example.com>
2026-07-02 17:02:07 -0300

    04: server, shared and database packages fix

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -3,9 +3,9 @@ import path from "path";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.ts";
 
-// dotenv.config({
-//   path: path.resolve(import.meta.dirname, "../../../.env"),
-// });
+dotenv.config({
+  path: path.resolve(import.meta.dirname, "../../../.env"),
+});
 
 const databaseUrl = process.env.DATABASE_URL;
 
--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod"
 import { findSupportedChatModel } from "@nightcode/shared";
-import { db } from "@nightcode/database";
+import { db } from "@nightcode/database/client";
 import { Role, Mode, MessageStatus } from "@nightcode/database/enums";
 
 const createSessionSchema = z.object({
